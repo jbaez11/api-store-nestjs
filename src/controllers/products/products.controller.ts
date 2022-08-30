e31278c1fb38b0815094e3bd020aba8fa7da6cd8
Jonathan Baez <jbaez9910@gmail.com>
2022-08-30 16:48:19 -0500

    implementacion de servicios

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -13,9 +13,11 @@ import {
 } from '@nestjs/common';
 
 import { response, Response } from 'express';
+import { ProductsService } from '../../services/products/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
   @Get()
   getProducts(
     @Query('limit') limit = 100,
@@ -23,7 +25,8 @@ export class ProductsController {
     @Query('brand') brand: string,
   ) {
     //limit=10&offset=50&brand=futbol
-    return { message: `product ${limit} ${offset} ${brand} ` };
+    //return { message: `product ${limit} ${offset} ${brand} ` };
+    return this.productsService.findAll();
   }
 
   @Get('filter')
@@ -32,28 +35,31 @@ export class ProductsController {
   }
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
-  getOne(@Res() response: Response, @Param('productId') productId: string) {
-    response.status(200).send({
-      message: `product ${productId}`,
-    });
+  getOne(@Param('productId') productId: string) {
+    // response.status(200).send({
+    //   message: `product ${productId}`,
+    // });
     // return {
     //   message: `product ${productId}`,
     // };
+    return this.productsService.findOne(+productId);
   }
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'accion de crear',
-      payload,
-    };
+    // return {
+    //   message: 'accion de crear',
+    //   payload,
+    // };
+    return this.productsService.create(payload);
   }
   @Put(':id')
-  uodate(@Param() id: number, @Body() payload: any) {
-    return {
-      message: 'accion de editar',
-      id,
-      payload,
-    };
+  update(@Param('id') id: string, @Body() payload: any) {
+    return this.productsService.update(+id, payload);
+    // return {
+    //   message: 'accion de editar',
+    //   id,
+    //   payload,
+    // };
   }
   @Delete(':id')
   delete(@Param() id: number) {
